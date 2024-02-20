52e3daaf70f08dcf40bf7593fea63d20d0e668a8
sebastian461 <sebastiano-461@hotmail.com>
2024-02-20 17:39:22 -0500

    HttpClient, IHttpClient y uso de appsetings como .env

---
+++ Backend/Controllers/PostController.cs
@@ -0,0 +1,23 @@
+﻿using Backend.DTOs;
+using Backend.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PostController : ControllerBase
+    {
+        private IPostService _titlesService;
+
+        public PostController(IPostService titlesService)
+        {
+            _titlesService = titlesService;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<PostDto>> Get() =>
+            await _titlesService.Get();
+    }
+}
---
+++ Backend/Services/IPostService.cs
@@ -0,0 +1,9 @@
+﻿using Backend.DTOs;
+
+namespace Backend.Services
+{
+    public interface IPostService
+    {
+        public Task<IEnumerable<PostDto>> Get();
+    }
+}
